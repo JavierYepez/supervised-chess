1e859dcd7a13728cdd4ea92234cf77ccff2c22ed
JavierYepez <javier.yepmar@gmail.com>
2024-08-29 09:55:10 +0200

    add a client to play against the AI

---
+++ play/app.py
@@ -0,0 +1,39 @@
+from contextlib import asynccontextmanager
+from pathlib import Path
+from fastapi import Depends, FastAPI, Request, Response
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+from .predict import load_model, predict_next_move
+
+
+model = load_model(Path("static/models/chess_white_14dim_run_20240826_1443.pth"))
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    model = load_model(Path("static/models/chess_white_14dim_run_20240826_1443.pth"))
+    yield
+    del model
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def load_index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html", context={})
+
+class BoardFen(BaseModel):
+    board_fen: str
+
+@app.post("/board")
+async def make_move(board_fen: BoardFen):
+    return predict_next_move(board_fen.board_fen, model)
+
---
+++ play/predict.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+
+import chess
+import torch
+from chess import Board, Move
+
+
+def load_model(model_path: Path) -> torch.nn.Module:
+    model = torch.jit.load(model_path)
+    model.to("cpu")
+    model.eval()
+    return model
+
+
+def board_to_matrix(board: Board) -> torch.Tensor:
+    matrix = torch.zeros(14, 8, 8, dtype=torch.float32)
+    piece_map = board.piece_map()
+
+    for square, piece in piece_map.items():
+        row, col = divmod(square, 8)
+        piece_type = piece.piece_type - 1
+        piece_color = 0 if piece.color else 6
+        matrix[piece_type + piece_color, row, col] = 1
+
+    legal_moves = board.legal_moves
+    for move in legal_moves:
+        from_square = move.from_square
+        row_from, col_from = divmod(from_square, 8)
+        matrix[12, row_from, col_from] = 1
+
+        to_square = move.to_square
+        row_to, col_to = divmod(to_square, 8)
+        matrix[13, row_to, col_to] = 1
+
+    matrix.to("cpu")
+    return matrix.unsqueeze(0)
+
+
+def decode_output(output_tensor: torch.Tensor, board: Board) -> Move:
+    tensor = output_tensor.squeeze(0)
+
+    from_square_list = torch.argsort(torch.softmax(tensor[:64], dim=0), descending=True)
+    to_square_list = torch.argsort(torch.softmax(tensor[64:], dim=0), descending=True)
+
+    for from_square in from_square_list:
+        piece = board.piece_at(from_square)
+        if piece is None or piece.color != board.turn:
+            continue
+        for to_square in to_square_list:
+            try:
+                return board.find_move(from_square, to_square)
+            except chess.IllegalMoveError as ex:
+                pass
+
+    return None
+
+
+def check_one_move_checkmate(board: Board):
+    checkmate_board = Board(board.fen())
+    for move in board.legal_moves:
+        checkmate_board.push(move)
+
+        if checkmate_board.is_checkmate():
+            return checkmate_board
+        checkmate_board.pop()
+
+
+def predict_next_move(fen_board: str, model: torch.nn.Module) -> Move:
+
+    board = Board(fen=fen_board)
+    if check_mate_board := check_one_move_checkmate(board):
+        return check_mate_board.fen()
+
+    input_tensor = board_to_matrix(board)
+
+    with torch.no_grad():
+        output_tensor = model(input_tensor)
+
+    move = decode_output(output_tensor, board)
+    board.push(move)
+
+    return board.fen()
